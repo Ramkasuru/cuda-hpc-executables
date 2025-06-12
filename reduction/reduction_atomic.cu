939b33d4bd42ef122e10985b68fc7270e7430a41
Kasuru Kodanda Rama Raju <127711765+Ramkasuru@users.noreply.github.com>
2025-06-11 20:23:44 -0700

    Update reduction_atomic.cu

--- reduction/reduction_atomic.cu
+++ reduction/reduction_atomic.cu
@@ -2,27 +2,28 @@
 #include <cuda_runtime.h>
 #define N 1024
 
-__global__ void reduce_atomic( const float* input, float* result) {
+__global__ void reduce_atomic(const float* input, float* result) {
     int i = threadIdx.x + blockIdx.x * blockDim.x;
     if (i < N) {
         atomicAdd(result, input[i]);    
     }
 }
-   void check_cuda_error(cudaError_t err) {
+
+void check_cuda_error(cudaError_t err) {
     if (err != cudaSuccess) {
         std::cerr << "CUDA error: " << cudaGetErrorString(err) << std::endl;
         exit(EXIT_FAILURE);
     } 
 }
 
-int main(){
+int main() {
     size_t size = N * sizeof(float);
-    float * h_input = new float[N];
+    float *h_input = new float[N];
 
     for (int i = 0; i < N; i++) {
         h_input[i] = 1.0f; // Initialize input array with 1.0
-
     }
+
     // Allocating memory on the device
     float *d_input, *d_result;
     check_cuda_error(cudaMalloc(&d_input, size));
